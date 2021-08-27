b56b77f447b9e27c4234ec4722b73e8f1a382d28
ch41rmn <chairman@mao.id.au>
2021-08-27 21:17:29 +1000

    fixing default tags

--- infra/main.tf
+++ infra/main.tf
@@ -17,23 +17,25 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-southeast-2"
+    region = "ap-southeast-2"
 
-  default_tags = {
-      "Project": "realms"
-  }
+    default_tags {
+        tags = {
+            Project = "realms"
+        }
+    }
 }
 
 data "aws_caller_identity" "current" {}
 
 output "account_id" {
-  value = data.aws_caller_identity.current.account_id
+    value = data.aws_caller_identity.current.account_id
 }
 
 output "caller_arn" {
-  value = data.aws_caller_identity.current.arn
+    value = data.aws_caller_identity.current.arn
 }
 
 output "caller_user" {
-  value = data.aws_caller_identity.current.user_id
+    value = data.aws_caller_identity.current.user_id
 }
