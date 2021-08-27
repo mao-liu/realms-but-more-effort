01f778d9278f0530831304881bc5351d97962c64
ch41rmn <chairman@mao.id.au>
2021-08-27 21:23:11 +1000

    fixing route table

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -18,3 +18,14 @@ resource "aws_subnet" "public" {
 resource "aws_internet_gateway" "igw" {
     vpc_id = aws_vpc.main.id
 }
+
+resource "aws_default_route_table" "main" {
+    default_route_table_id = aws_vpc.main.default_route_table_id
+
+    route = [
+        {
+            cidr_block = "0.0.0.0/0"
+            gateway_id = aws_internet_gateway.igw.id
+        }
+    ]
+}
