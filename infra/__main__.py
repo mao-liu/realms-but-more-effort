3cefbc97b238ae2d761ea8427500ba1d66674d65
ch41rmn <chairman@mao.id.au>
2021-08-27 20:50:21 +1000

    pivoting to terraform cloud

--- infra/__main__.py
+++
@@ -1,3 +0,0 @@
-from . import infra
-
-infra.main()
---
+++ infra/main.tf
@@ -0,0 +1,35 @@
+terraform {
+    backend "remote" {
+        hostname     = "app.terraform.io"
+        organization = "mao-liu"
+
+        workspaces {
+            name = "game-1"
+        }
+    }
+
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
