d6f9d9b558d67047ddacb1c01977008b458402fa
ch41rmn <chairman@mao.id.au>
2021-08-28 22:30:39 +1000

    fixing broken tf

--- infra/s3.tf
+++ infra/s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "realms" {
     bucket = "realms-but-more-effort"
 }
 
-resource "aws_ssm_paramter" "world_s3_path" {
+resource "aws_ssm_parameter" "world_s3_path" {
     name  = "/realms/outputs/world_s3_path"
     type  = "String"
     value = "s3://${aws_s3_bucket.realms.id}/worlds/server.tar.gz"
